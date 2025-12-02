7a4ac45eabb8d032bba5515d8656f5ff58953980
Cedric <crubbertire@rogers.com>
2025-12-02 14:29:55 -0500

    Schema/DB connection

--- locations_api/models.py
+++ locations_api/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Location(models.Model):
-  street = 
-  city = 
-  state = 
+  street_number = models.IntegerField()
+  street = models.CharField(max_length=32)
+  city = models.CharField(max_length=48)
+  state = models.CharField(max_length=14)

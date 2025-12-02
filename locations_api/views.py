2944fcc6984d5bb2eb49f46373fa92c008be77c6
Cedric <crubbertire@rogers.com>
2025-12-02 14:42:37 -0500

    Created Views

--- locations_api/views.py
+++ locations_api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from .serualizers import LocationSerializer
+from .models import Location
+
+class LocationList(generics.ListCreateAPIView):
+  queryset = Location.objects.all().order_by("id")
+  serializer_class = LocationSerializer
+
+class LocationDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Location.objects.all().order_by("id")
+  serializer_class = LocationSerializer
